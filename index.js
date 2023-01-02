f5069b19c6cbdbda34a24260e90469260c71be06
Jehad Almaliki <yourgitemail@example.com>
2023-01-02 10:50:07 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code here
+function moveDodgerLeft() {
+  var dodger = document.getElementById("dodger");
+  dodger.style.left = (dodger.offsetLeft - 10) + "px";
+}
+
+function moveDodgerRight() {
+  var dodger = document.getElementById("dodger");
+  dodger.style.left = (dodger.offsetLeft + 10) + "px";
+}
